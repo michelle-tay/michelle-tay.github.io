078dec8a02ab7e18161a19326ef650a1603086d5
michelle-tay <89303604+michelle-tay@users.noreply.github.com>
2023-09-04 21:37:32 +0800

    Update main.js (pause play music)

--- appscripts/main.js
+++ appscripts/main.js
@@ -6,8 +6,24 @@ function windowprompt(){
     alert("Thank you for visting my portfolio! \nDo note that this site will still be going through changes! (ꈍᴗꈍ) \n\n-Michelle");
   }
 
+  // to execute function upon opening loading the page
+  window.addEventListener('load', function(){
+    // get the element id of myAudio
+    let myAudio = document.getElementById("myAudio");
+    
+    //set isplaying to true on play
+    myAudio.onplaying = function() {
+      isPlaying = true;
+    };
 
-//let is playing to be set to false at first so music doesnt play first unless button is pressed.
+    //set isplaying to false on pause 
+    myAudio.onpause = function() {
+      isPlaying = false;
+    };
+});
+
+
+  //let is playing to be set to false at first so music doesnt play first unless button is pressed.
 let isPlaying = false;
 
 // declare function to toggle between play and pause
