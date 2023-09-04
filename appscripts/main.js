8cefe794cc8d082ed26eb51f1bfb66fa69a1f25e
michelle-tay <89303604+michelle-tay@users.noreply.github.com>
2023-09-04 21:30:45 +0800

    Update main.js (add function audio player)

--- appscripts/main.js
+++ appscripts/main.js
@@ -5,3 +5,16 @@ function jumptoabout(){
 function windowprompt(){ 
     alert("Thank you for visting my portfolio! \nDo note that this site will still be going through changes! (ꈍᴗꈍ) \n\n-Michelle");
   }
+
+
+//let is playing to be set to false at first so music doesnt play first unless button is pressed.
+let isPlaying = false;
+
+// declare function to toggle between play and pause
+function togglePlay() {
+    if (isPlaying) {
+        myAudio.pause()
+    } else {
+        myAudio.play();
+    }}
+
